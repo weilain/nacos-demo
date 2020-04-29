747c451e55b0562ac8b2c9a4d62fa258476e9293
carzy <carzy.dong@tonmx.com>
2020-04-29 12:04:48 +0800

    fix: nacos配置移至配置中心

--- nacos-server-a/src/main/java/com/dxp/discovery/NacosDiscoveryApplication.java
+++ nacos-server-a/src/main/java/com/dxp/discovery/NacosDiscoveryApplication.java
@@ -1,20 +1,17 @@
 package com.dxp.discovery;
 
-import com.alibaba.boot.nacos.discovery.autoconfigure.NacosDiscoveryAutoConfiguration;
-import com.alibaba.boot.nacos.discovery.properties.NacosDiscoveryProperties;
-import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySources;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author carzy
  */
 @SpringBootApplication
-@EnableNacosDiscovery
-@EnableConfigurationProperties(NacosDiscoveryProperties.class)
-@Import(NacosDiscoveryAutoConfiguration.class)
+@NacosPropertySources({
+        @NacosPropertySource(dataId = "server-a.properties", groupId = "demo", name = "carzy"),
+})
 public class NacosDiscoveryApplication {
 
     public static void main(String[] args) {
--- nacos-server-b/src/main/java/discovery/Application.java
+++ nacos-server-b/src/main/java/discovery/Application.java
@@ -1,20 +1,14 @@
 package discovery;
 
-import com.alibaba.boot.nacos.discovery.autoconfigure.NacosDiscoveryAutoConfiguration;
-import com.alibaba.boot.nacos.discovery.properties.NacosDiscoveryProperties;
-import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author carzy
  */
 @SpringBootApplication
-@EnableNacosDiscovery
-@EnableConfigurationProperties(NacosDiscoveryProperties.class)
-@Import(NacosDiscoveryAutoConfiguration.class)
+@NacosPropertySource(dataId = "server-a.properties", groupId = "demo")
 public class Application {
 
     public static void main(String[] args) {
