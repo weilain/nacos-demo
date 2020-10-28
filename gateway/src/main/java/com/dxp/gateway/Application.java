3e650ba2b9973b87ab4a943dee1c442f55163cd8
carzy <carzy.dong@tonmx.com>
2020-10-28 09:53:07 +0800

    fix: 网关路由规则查看

--- gateway/src/main/java/com/dxp/gateway/Application.java
+++ gateway/src/main/java/com/dxp/gateway/Application.java
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 /**
  * @author gaohui
@@ -21,25 +20,15 @@ import reactor.core.publisher.Mono;
 public class Application {
 
     @Autowired
-    private RouteLocator routeLocator;
+    private RouteDefinitionLocator routeDefinitionLocator;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @GetMapping("routes")
-    public Mono<RouteLocator> getRouteLocator() {
-        return Mono.just(routeLocator);
-    }
-
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                // 配制一个路由,把 http://网关地址:网关端口/demo/ 下的请求路由到 demo-service 微服务中
-                .route(p -> p
-                        .path("/sever-a/**") //url匹配
-                        .uri("lb://nacos-server-a"))  // 将请求路由到指定目标, lb开头是注册中心中的服务, http/https 开头你懂的
-                .build();
+    public Flux<RouteDefinition> getRouteLocator() {
+        return routeDefinitionLocator.getRouteDefinitions();
     }
 
 }
